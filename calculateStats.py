434631a425abdee5e3dab7f9c170a41b026f296d
black-pine <rsmats13@gmail.com>
2021-04-04 13:18:34 -0400

    Minor changes to issue type and series data

--- calculateStats.py
+++ calculateStats.py
@@ -63,19 +63,19 @@ with open('comicsData.csv', 'r') as comics:
 			# add issue type to count by publisher
 			if not row['issue']:
 				publisherSales[row['publisher']]['types']['oneShot'] += 1
-			elif row['issue'] == 1:
+			elif row['issue'] == '1':
 				publisherSales[row['publisher']]['types']['numOne'] += 1
 			else:
 				publisherSales[row['publisher']]['types']['ongoing'] += 1
 		
 		# add issue sales amount and issue number to total by series
 		if row['title'] not in seriesSales:
-			seriesSales['title'] = {
+			seriesSales[row['title']] = {
 				'issues' : set(),
 				'income' : 0,
 			}
-		seriesSales['title']['income'] += salesIncome
-		seriesSales['title']['issues'].add(row['issue'])
+		seriesSales[row['title']]['income'] += salesIncome
+		seriesSales[row['title']]['issues'].add(row['issue'])
 
 		# add issue sales amount to the monthly sales total
 		monthlySales[row['month']] += salesIncome
@@ -83,14 +83,69 @@ with open('comicsData.csv', 'r') as comics:
 		if row['reorder']:
 			if row['item code'] not in reorders:
 				reorders[row['item code']] = {
-					'title': row['title'],
+					'series': row['title'],
+					'issue': row['issue'],
 					'count': 0,
 					'units': 0
 				}
-				if row['issue']:
-					reorders[row['item code']]['title'] += ' iss. ' + row['issue']
 			reorders[row['item code']]['count'] += 1
 			reorders[row['item code']]['units'] += int(row['estimated sales'])
 
-	# print(publisherSales)
-	# print(round(publisherSales['Image Comics']['income'], 2))
+
+with open('publisherData.csv', 'w') as publisherInfo:
+	publisherHeader=['publisher', 'year', 'units', 'issues', 'income']
+	w = csv.DictWriter(publisherInfo, fieldnames=publisherHeader)
+	w.writeheader()
+	for pub in publisherSales:
+		row = { 'publisher': pub }
+		for year in publisherSales[pub]:
+			if year == 'types':
+				break
+			row['year'] = year
+			row['units'] = publisherSales[pub][year]['units']
+			row['issues'] = publisherSales[pub][year]['issues']
+			row['income'] = publisherSales[pub][year]['income']
+			w.writerow(row)
+
+with open('issueTypeData.csv', 'w') as typeInfo:
+	typeHeader=['publisher', 'one shot', 'first issue', 'ongoing']
+	w = csv.DictWriter(typeInfo, fieldnames=typeHeader)
+	w.writeheader()
+	for pub in publisherSales:
+		row = {
+			'publisher' : pub,
+			'one shot' : publisherSales[pub]['types']['oneShot'],
+			'first issue' : publisherSales[pub]['types']['numOne'],
+			'ongoing' : publisherSales[pub]['types']['ongoing']
+		}
+		w.writerow(row)
+
+with open('seriesData.csv', 'w') as seriesInfo:
+	seriesHeader=['title', 'total issues', 'income']
+	w = csv.DictWriter(seriesInfo, fieldnames=seriesHeader)
+	w.writeheader()
+	for series in seriesSales:
+		row = {
+			'title' : series,
+			'total issues' : len(seriesSales[series]['issues']),
+			'income' : seriesSales[series]['income']
+		}
+		w.writerow(row)
+
+with open('reorderData.csv', 'w') as reorderInfo:
+	reorderHeader=['series', 'issue', 'count', 'units']
+	w = csv.DictWriter(reorderInfo, fieldnames=reorderHeader)
+	w.writeheader()
+	for item in reorders:
+		w.writerow(reorders[item])
+
+with open('monthlyData.csv', 'w') as monthlyInfo:
+	monthlyHeader=['month', 'income']
+	w = csv.DictWriter(monthlyInfo, fieldnames=monthlyHeader)
+	w.writeheader()
+	for month in monthlySales:
+		row = {
+			'month' : month,
+			'income' : monthlySales[month]
+		}
+		w.writerow(row)
